17b96f03eaad443a6e6278ab4dae0c7e56792d1a
EA Rashel <rashel320@gmail.com>
2017-10-21 13:45:33 +0600

    add molten sdk using pods

--- EA_STORE/ViewController.swift
+++ EA_STORE/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import Moltin
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        Moltin.sharedInstance().setPublicId("4kVFVU1jaih3FYehPsnVbS22CWVgT7AdbJyOLIUEKC")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
